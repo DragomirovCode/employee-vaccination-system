46c66e7c7d6259f85bea9bf31f1d642e749741d1
DragomirovCode <103379771+DragomirovCode@users.noreply.github.com>
2026-03-01 23:09:00 +0300

    MERGED: employee | feature | #17 - [БЭК] Реализовать модуль employee (Departments + Employees) строго по docs

--- scripts/check_commit.sh
+++ scripts/check_commit.sh
@@ -12,7 +12,7 @@ NC='\033[0m'
 # Берём только первую строку commit message
 COMMIT_MESSAGE="$(git log -1 --pretty=%B | head -n 1)"
 
-MODULES="generic|auth"
+MODULES="generic|auth|employee"
 TYPES="feature|fix|docs|refactor|perf|test|config|security"
 
 COMMIT_MASK="^MERGED:[[:space:]](${MODULES})[[:space:]]\|[[:space:]](${TYPES})[[:space:]]\|[[:space:]]\#[[:digit:]]+[[:space:]]-[[:space:]].+"
