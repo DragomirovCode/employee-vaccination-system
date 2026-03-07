df5ddbe902834151cf58b5807f797d13e653227d
Misha Dragomirov <mikhail.dragomirov.02@gmail.com>
2026-03-07 14:45:28 +0300

    try

--- backend/reporting/src/main/kotlin/com/example/reporting/revaccination/RevaccinationDueController.kt
+++ backend/reporting/src/main/kotlin/com/example/reporting/revaccination/RevaccinationDueController.kt
@@ -2,7 +2,6 @@
 
 import com.example.reporting.access.ReportingAccessScope
 import com.example.reporting.access.ReportingSecurityContext
-import jakarta.servlet.http.HttpServletRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.ArraySchema
@@ -11,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
