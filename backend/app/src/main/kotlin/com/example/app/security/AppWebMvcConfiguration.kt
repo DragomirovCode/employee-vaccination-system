9d2474bf40fb1749db9029ea0391ce025089a05f
Misha Dragomirov <mikhail.dragomirov.02@gmail.com>
2026-04-18 18:08:12 +0300

    try

--- backend/app/src/main/kotlin/com/example/app/security/AppWebMvcConfiguration.kt
+++
@@ -1,7 +0,0 @@
-﻿package com.example.app.security
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-@Configuration
-class AppWebMvcConfiguration : WebMvcConfigurer
